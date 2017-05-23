02381fa5582e9ae1c4deef33605b70a0fa0dd85e
chihao <chihaoyo@gmail.com>
2017-05-23 21:34:21 +0800

    Add DataStore; use DataStore in template

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+<footer class="standard">
+  <div class="land">{{ DataStore.links.home }}</div>
+</footer>
+</template>
+
+<script>
+import common from '../main'
+
+export default {
+  name: 'footer-standard',
+  data: function() {
+    return {
+      DataStore: common.DataStore
+    }
+  }
+}
+</script>
