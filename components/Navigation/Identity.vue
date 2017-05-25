c51eea548a1377dafbaca9ceb8bb6371a7122072
chihao <chihaoyo@gmail.com>
2017-05-26 00:05:54 +0800

    Add navbar-identity

---
+++ components/Navigation/Identity.vue
@@ -0,0 +1,39 @@
+<template>
+<nav class="navbar sticky-top navbar-light" :class="channel.classes.bg">
+  <a class="navbar-brand" :href="channel.links.home"><img :src="channel.asset.logoWithType.regular.src" class="logo-type" :style="channel.asset.logoWithType.regular.style"/></a>
+  <a class="navbar-button" id="navbar-identity" @click="promptAuth">Login</a>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'navigation-with-identity',
+  mounted() {
+    console.log('#' + this.channel.id);
+    console.log('Navigation with identity initiated.');
+  },
+  data() {
+    return {
+      channel: this.$parent.$data.channel
+    }
+  },
+  methods: {
+    promptAuth() {
+      var $ = global.$
+      $('#modal-auth').modal('show')
+    }
+  }
+}
+</script>
+
+<style>
+.navbar-button {
+  display: block;
+  width: 60px;
+  height: 64px;
+}
+#navbar-identity {
+  position: absolute;
+  right: 0;
+}
+</style>
