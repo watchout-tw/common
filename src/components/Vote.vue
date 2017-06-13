5b0eab4282670f68322a1f90cd12507a309b9cde
chihao <chihaoyo@gmail.com>
2017-06-13 14:36:48 +0800

    Add Vote

---
+++ src/components/Vote.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="vote">
+  <div v-for="option in options">{{ option.name }}</div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: 'people',
+      options: [
+        {
+          id: 'aaa',
+          name: 'AAA'
+        },
+        {
+          id: 'bbb',
+          name: 'BBB'
+        },
+        {
+          id: 'ccc',
+          name: 'CCC'
+        },
+        {
+          id: 'ddd',
+          name: 'DDD'
+        },
+        {
+          id: 'eee',
+          name: 'EEE'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
